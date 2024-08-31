415b161f6a3a47043103a44cc4af0681e73e6bf3
treemanrocket <56743498+treemanrocket@users.noreply.github.com>
2024-08-31 08:52:31 -0400

    inventory script stuff

--- MysticBound/Assets/Scripts/Singleton_scripts/InventoryScript.cs
+++ MysticBound/Assets/Scripts/Singleton_scripts/InventoryScript.cs
@@ -18,6 +18,20 @@ public class InventoryScript : MonoBehaviour
         instance = this;
     }
     #endregion
+
+    #region /*INVENTORY*/
+    [Header("INVENTORY")]
+    [Tooltip("limit for how many items you can have in the inventory")][SerializeField] private int inventoryLimit;
+    [System.Serializable]
+    public struct inventoryData
+    {
+        public Sprite inventoryDataSprite;
+        public GameObject inventoryDataGameObject;
+        //public GunBehavior armGunBehavior;
+    }
+    [Tooltip("a list of how many items")] public List<inventoryData> itemList = new List<inventoryData>();
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
