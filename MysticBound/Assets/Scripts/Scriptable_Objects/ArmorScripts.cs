f5df76314cd2ea7a9ea8c34a8090ea11709fedff
treemanrocket <56743498+treemanrocket@users.noreply.github.com>
2024-08-31 13:41:22 -0400

    created scriptable objects
    made some scripts

---
+++ MysticBound/Assets/Scripts/Scriptable_Objects/ArmorScripts.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Gear System/Armors", order = 12)]
+public class ArmorScripts : ScriptableObject
+{
+    public enum ArmorType
+    {
+        HEAD,
+       CHEST,
+        LEGS,
+        FEET
+    }
+
+    [Header("Armor Type")]
+    public ArmorType armorType;
+
+    [Header("Stats")]
+    public int armorValue;
+
+}
---
+++ MysticBound/Assets/Scripts/Scriptable_Objects/WeaponScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Gear", menuName = "Gear System/Weapons", order = 11)]
+
+public class WeaponScript : ScriptableObject
+{
+    public enum WeaponType
+    {
+        LONGSWORD,
+        AXE,
+        GAUNTLETS,
+        NUNCHAKU
+    }
+
+    public enum WeaponElementType
+    {
+        NONE,
+        WATER,
+        FIRE,
+        EARTH,
+        LIGHTNING        
+    }
+
+    
+    [Header("Weapon Type/ Element")]
+    [Tooltip("This is the weapon type, just select the drop down menu for what weapon type it wil be")]
+    public WeaponType weaponType;
+
+    [Tooltip("This is the weapon elements if they have any")]
+    public WeaponElementType weaponElementType;
+
+    [Header("Stats")]
+    public int damage;
+    public int attackSpeed;
+
+}
